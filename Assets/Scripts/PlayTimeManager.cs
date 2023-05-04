74a88b571eca86fa49d2cad6ca29917a1a2e0eee
Tackiee <takitt1720@gmail.com>
2023-05-04 13:21:41 +0900

    カウントダウン実装・一定時間停止後End_Sceneheno

--- Assets/Scripts/PlayTimeManager.cs
+++ Assets/Scripts/PlayTimeManager.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayTimeManager : MonoBehaviour
 {
     public CountDown countDown;
-    float timer = 20.0f;
+    public Text TimerText;
+
+    float timer = 10.0f;
+    float stay = 2.0f;
     bool checker;
+    bool GameFinish = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +22,27 @@ public class PlayTimeManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         Debug.Log(timer);
         checker = countDown.startChecker;
-        if (checker)
+        if (checker && !GameFinish)
         {
+            TimerText.text = timer.ToString();
             timer -= Time.deltaTime;
             if (timer < 0)
             {
+                GameFinish = true;
                 timer = 0.0f;
+            }
+        }
+        
+        if(GameFinish)
+        {
+            stay -= Time.deltaTime;
+            TimerText.text = "I—¹I";
+            if(stay < 0)
+            {
+                SceneManager.LoadScene("End_Scene");
             }
         }
     }
