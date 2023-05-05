751b00f3938bf9d9d3f70d3ee52951af591b3474
Tackiee <takitt1720@gmail.com>
2023-05-05 13:20:29 +0900

    ジャンプの発生条件修正・文字送りとりあえず完成

--- Assets/Scripts/StoryTelling_Script.cs
+++ Assets/Scripts/StoryTelling_Script.cs
@@ -6,17 +6,30 @@ using UnityEngine.UI;
 public class StoryTelling_Script : MonoBehaviour
 {
     [SerializeField] Text zunda_text;
+
+    private bool click;
     private string[] wordArray;
     private string words = "今,日,は,と,て,も,天,気,が,い,い,の,だ,\n";
+    public GameObject Button;
+    public GameObject[] zundaPic;
 
-    private string[] zunda_word;
+    private string[] zunda_word = { "今日はとても天気がいいのだ" ,
+                                    "こんな日はずんだもちを食べてお花見するのに限るのだ",
+                                    "世間はかしわもちを食えとかほざくけど、頭が沸いてるとしか思えないのだ",
+                                    "だけど、ずんだもちが花びらまみれになったら台無しなのだ",
+                                    "さくらの花びらがつかないようにして、おいしいずんだもちを食べたいのだ"
+                                  };
     private string show_word;
+
+    int i;
+    
 
     int say_num = 0;
 
     void Start()
     {
-        words = "こ,こ,か,ら,先,は,2,つ,に,道,が,分,か,れ,て,い,る,よ,。,\n";
+        Button.SetActive(false);
+        StartCoroutine(DisplaySentence());
     }
 
     void Update()
@@ -33,12 +46,24 @@ public class StoryTelling_Script : MonoBehaviour
         }
     }
 
-    IEnumerator SetText()
+    IEnumerator DisplaySentence()
     {
-        foreach (var p in wordArray)
+        foreach(char x in zunda_word[i].ToCharArray())
         {
-            zunda_text.text = zunda_text.text + p;
+            zunda_text.text += x; //1文字ずつ追加
             yield return new WaitForSeconds(0.1f);
+        }
+        Button.SetActive(true);
+    }
+
+    public void OnClickButton()
+    {
+        Button.SetActive(false);
+        if (i < zunda_word.Length - 1) //最後の文章でないなら
+        {
+            i++;
+            zunda_text.text = ""; //現在の文章を白紙に
+            StartCoroutine(DisplaySentence()); //次の文字送り開始
         }
     }
 }
